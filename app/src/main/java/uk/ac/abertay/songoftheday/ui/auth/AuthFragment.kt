da05a7d719005ebd4507277a8dd45557cb5b6eb4
Waveyyyy <waves-sec@protonmail.com>
2023-05-14 21:08:10 +0100

    Added login functionality

--- app/src/main/java/uk/ac/abertay/songoftheday/ui/auth/AuthFragment.kt
+++ app/src/main/java/uk/ac/abertay/songoftheday/ui/auth/AuthFragment.kt
@@ -6,31 +6,72 @@ import androidx.fragment.app.Fragment
 import uk.ac.abertay.songoftheday.R
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import uk.ac.abertay.songoftheday.databinding.FragmentAuthBinding
-import uk.ac.abertay.songoftheday.databinding.FragmentCalendarBinding
 
 class AuthFragment : Fragment() {
 
-    private var _binding: FragmentAuthBinding? = null
+    private lateinit var binding: FragmentAuthBinding
 
-    private val binding get() = _binding!!
+    private lateinit var authViewModel: AuthViewModel
 
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View
     {
-        val authViewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
+        authViewModel = ViewModelProvider(this)[AuthViewModel::class.java]
 
-        _binding = FragmentAuthBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_auth, container, false)
 
+        auth = Firebase.auth
 
-
-        return root
+/*
+        TODO: Decide how to do signups
+*/
+        return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.resetButton.setOnClickListener {
+            clearInput()
+        }
+        binding.submitButton.setOnClickListener {
+            if (auth.currentUser == null) {
+                submitLoginDetails()
+            }
+        }
+
+
     }
 
+    private fun submitLoginDetails() {
+            val email = binding.emailField.text.toString().trim()
+            val password = binding.passwordField.text.toString().trim()
+            Snackbar.make(binding.root, "Username:$email + Password:$password", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+            if (authViewModel.logUserIn(email, password, auth))
+            {
+                Snackbar.make(binding.root, "Authentication Successful", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+            } else {
+                Snackbar.make(binding.root, "Authentication Failed", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+            }
+    }
+
+    private fun clearInput() {
+        binding.emailField.setText("")
+        binding.passwordField.setText("")
+        /*TODO: add logging*/
+    }
+
+    override fun onDestroy() {
+        clearInput()
+        super.onDestroy()
+        /*TODO: add logging*/
+    }
 }
--- app/src/main/java/uk/ac/abertay/songoftheday/ui/auth/AuthViewModel.kt
+++ app/src/main/java/uk/ac/abertay/songoftheday/ui/auth/AuthViewModel.kt
@@ -1,7 +1,17 @@
 package uk.ac.abertay.songoftheday.ui.auth
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class AuthViewModel() : ViewModel() {
+
+    fun logUserIn(email: String, password: String, auth: FirebaseAuth): Boolean
+    {
+        var success: Boolean = false
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener{ task ->
+            success = task.isSuccessful
+            }
+        return success
+    }
 
 }
