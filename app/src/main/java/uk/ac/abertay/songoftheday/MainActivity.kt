4bfc9c77cf0d02d56ebe43ce02bac9f01738b911
Waveyyyy <waves-sec@protonmail.com>
2023-05-14 06:58:52 +0100

    add navigation for login and profile fragments
    also adds checklogin function which does the same as the one in homefragment.kt except with the menu items

--- app/src/main/java/uk/ac/abertay/songoftheday/MainActivity.kt
+++ app/src/main/java/uk/ac/abertay/songoftheday/MainActivity.kt
@@ -1,7 +1,10 @@
 package uk.ac.abertay.songoftheday
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -10,7 +13,12 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import uk.ac.abertay.songoftheday.databinding.ActivityMainBinding
+import uk.ac.abertay.songoftheday.ui.auth.AuthFragment
+import uk.ac.abertay.songoftheday.ui.profile.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -43,6 +51,31 @@ class MainActivity : AppCompatActivity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_profile)
+        {
+            val navctrl :NavController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main)
+            navctrl.navigate(R.id.nav_profile)
+        }
+        else if (item.itemId == R.id.action_login)
+        {
+            val navctrl :NavController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main)
+            navctrl.navigate(R.id.nav_auth)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun checkLoggedIn(menu:Menu) {
+        val login = null
+        if (login == null)
+        {
+            menu.findItem(R.id.action_login).setVisible(true)
+            menu.findItem(R.id.action_profile).setVisible(false)
+        }
+        menu.findItem(R.id.action_login).setVisible(false)
+        menu.findItem(R.id.action_profile).setVisible(true)
     }
 
     override fun onSupportNavigateUp(): Boolean {
