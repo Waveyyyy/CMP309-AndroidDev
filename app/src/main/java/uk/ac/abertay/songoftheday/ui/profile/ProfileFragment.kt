99f7c1f6169cc4290f7c4fc4789879bacc7f420b
Waveyyyy <waves-sec@protonmail.com>
2023-05-15 01:39:26 +0100

    add logout fucntionality and removed profile picture

--- app/src/main/java/uk/ac/abertay/songoftheday/ui/profile/ProfileFragment.kt
+++ app/src/main/java/uk/ac/abertay/songoftheday/ui/profile/ProfileFragment.kt
@@ -6,31 +6,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import uk.ac.abertay.songoftheday.MainActivity
 import uk.ac.abertay.songoftheday.R
 import uk.ac.abertay.songoftheday.databinding.FragmentProfileBinding
-import uk.ac.abertay.songoftheday.ui.auth.AuthViewModel
 
 class ProfileFragment : Fragment() {
 
-    private var _binding: FragmentProfileBinding? = null
+    private lateinit var binding: FragmentProfileBinding
 
-    private val binding get() = _binding!!
+    private lateinit var profileViewModel: ProfileViewModel
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View
     {
-        val authViewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
+        profileViewModel = ViewModelProvider(this)[ProfileViewModel::class.java]
 
-        _binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
 
+        auth = Firebase.auth
 
-
-        return root
+        return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.logoutFab.setOnClickListener {
+            if (auth.currentUser != null) {
+                logUserOut(view)
+            }
+        }
+    }
+
+    private fun logUserOut(view: View) {
+        if (profileViewModel.logOut(auth))
+        {
+            view.findNavController().navigate(R.id.action_nav_profile_to_nav_auth)
+        } else {
+            Snackbar.make(binding.root, "Logout Failed", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+        }
     }
 
 }
--- app/src/main/java/uk/ac/abertay/songoftheday/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/uk/ac/abertay/songoftheday/ui/profile/ProfileViewModel.kt
@@ -1,7 +1,15 @@
 package uk.ac.abertay.songoftheday.ui.profile
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    fun logOut(auth: FirebaseAuth): Boolean {
+        auth.signOut()
+        if (auth.currentUser == null){
+            return true
+        }
+        return false
+    }
 }
