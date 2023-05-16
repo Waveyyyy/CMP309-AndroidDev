71c05e4e173d5dd801aced581eb563ad36ae5fb9
Waveyyyy <waves-sec@protonmail.com>
2023-05-16 10:42:41 +0100

    spotify playlist now created correctly and songs can be added
    could not get duplicate track checking to work unforch but L + ratio + cope + seethe
    
    also forgor to commit a bunch oftheese files earlier

--- app/src/main/java/uk/ac/abertay/songoftheday/ui/home/HomeFragment.kt
+++ app/src/main/java/uk/ac/abertay/songoftheday/ui/home/HomeFragment.kt
@@ -12,7 +12,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.adamratzman.spotify.*
 import com.adamratzman.spotify.auth.pkce.startSpotifyClientPkceLoginActivity
-import com.adamratzman.spotify.models.Playlist
+import com.adamratzman.spotify.models.*
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -26,7 +26,7 @@ import uk.ac.abertay.songoftheday.databinding.FragmentHomeBinding
 import uk.ac.abertay.songoftheday.spotify.SpotifyPkceLoginActivityImpl
 
 
-class HomeFragment : Fragment(){
+class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
 
@@ -58,37 +58,46 @@ class HomeFragment : Fragment(){
         super.onViewCreated(view, savedInstanceState)
         checkLoggedIn()
         binding.root.findViewById<View>(R.id.fab_home).setOnClickListener { view ->
-                if (Model.credentialStore.spotifyToken == null)
-                {
-                    Log.i("HomeFragment", "Re-Authentication is needed")
-                    Snackbar.make(view, "Re-Authenticatin Needed", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show()
-                    activity?.startSpotifyClientPkceLoginActivity(SpotifyPkceLoginActivityImpl::class.java)
-                    Snackbar.make(view, "Authentication Successful", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show()
-                }else {
-                    lifecycleScope.launch {
-                        val sotdPlaylist = playlistExistsAsync().await()
-                    }
+            if (Model.credentialStore.spotifyToken == null) {
+                Log.i("HomeFragment", "Re-Authentication is needed")
+                Snackbar.make(view, "Re-Authenticatin Needed", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+                activity?.startSpotifyClientPkceLoginActivity(SpotifyPkceLoginActivityImpl::class.java)
+                Snackbar.make(view, "Authentication Successful", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+            } else {
+                lifecycleScope.launch {
+                    val sotdPlaylist = playlistExistsAsync(view).await()
                     searchTrackDialog(view)
                 }
             }
         }
+    }
 
-    private fun playlistExistsAsync(): Deferred<Playlist?> = lifecycleScope.async {
+    private fun playlistExistsAsync(view: View): Deferred<Playlist?> = lifecycleScope.async {
         val playlistName = "Song of the Day app"
-        var sotdPlaylist: Playlist?
+        var sotdPlaylist: Playlist? = null
         val api = Model.credentialStore.getSpotifyClientPkceApi()
 
-        if (api!!.token.expiresAt < System.currentTimeMillis()){
+        if (api!!.token.expiresAt < System.currentTimeMillis()) {
             api.refreshToken()
         }
 
-        sotdPlaylist = api.playlists.getPlaylist(playlistName)
-        if (sotdPlaylist?.id == "") {
-                createSOTDPlaylist(api, playlistName)
-                sotdPlaylist = api.playlists.getPlaylist(playlistName)
-        }else {
+        var sotdPlaylistId: String? = null
+        api.playlists.getClientPlaylists().items.forEach {
+            if (it.name == playlistName) {
+                sotdPlaylistId = it.id
+                return@forEach
+            }
+        }
+
+        Log.d("HomeFragment", "playlistId: $sotdPlaylistId")
+        if (sotdPlaylistId == null) {
+            createSOTDPlaylist(api, playlistName)
+            sotdPlaylist = api.playlists.getPlaylist(playlistName)
+            Snackbar.make(view, "Playlist Created", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        } else {
             Log.i("HomeFragment", "Playlist $playlistName exists")
         }
         return@async sotdPlaylist
@@ -104,48 +113,80 @@ class HomeFragment : Fragment(){
         val builder = AlertDialog.Builder(requireContext())
         val inflater = layoutInflater
         builder.setTitle("Search for a song")
-        val dialogLayout = inflater.inflate(R.layout.dialog_search_song ,null)
+        val dialogLayout = inflater.inflate(R.layout.dialog_search_song, null)
         val editText = dialogLayout.findViewById<EditText>(R.id.search_song)
         builder.setView(dialogLayout)
-        builder.setPositiveButton("Search") {
-                _, _ -> lifecycleScope.launch {searchResults(editText, view)}
+        builder.setPositiveButton("Search") { _, _ ->
+            lifecycleScope.launch { searchResults(editText, view) }
         }
         builder.show()
     }
 
-    private suspend fun searchResults(searchString: EditText, view: View)
-    {
-        val api = Model.credentialStore.getSpotifyClientPkceApi()
+    private suspend fun searchResults(searchString: EditText, view: View) {
+        if (searchString.text.toString().isEmpty()) {
+            searchTrackDialog(view)
+        } else {
+            val api = Model.credentialStore.getSpotifyClientPkceApi()
+            var trackUris = emptyArray<PlayableUri>()
 
-        if (api!!.token.expiresAt < System.currentTimeMillis()){
-            api.refreshToken()
+            if (api!!.token.expiresAt < System.currentTimeMillis()) {
+                api.refreshToken()
+            }
+            val tracks = withContext(Dispatchers.IO) {
+                val searchRes = api.search.searchTrack(searchString.text.toString().trim())
+                val tracks = searchRes.items.map { it.name }.toTypedArray()
+                trackUris = searchRes.items.map { it.uri }.toTypedArray()
+                Log.d("HomeFragment", "Search Query: ${searchString.text.toString()}")
+                Log.d("HomeFragment", "Search Result: ${searchRes.items}")
+                Log.d("HomeFragment", "Tracks: ${tracks.joinToString(", ")}")
+                tracks
+            }
+            val builder = AlertDialog.Builder(requireContext())
+            with(builder) {
+                setTitle("Search Result")
+                setItems(tracks) { _, item ->
+                    lifecycleScope.launch {
+                        addTrackToPlaylist(tracks[item], api, trackUris[item], view)
+/*
+                        Snackbar.make(view, "${tracks[item]}", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show()
+*/
+                    }
+                }
+            }
+            builder.show()
         }
-        val tracks = withContext(Dispatchers.IO) {
-            val searchRes = api.search.searchTrack(searchString.text.toString().trim())
-            val tracks = searchRes.items.map { it.name }.toTypedArray()
-            Log.d("HomeFragment", "Search Query: ${searchString.text.toString()}")
-            Log.d("HomeFragment", "Search Result: ${searchRes.items}")
-            Log.d("HomeFragment", "Tracks: ${tracks}")
-            tracks
-        }
-        val builder = AlertDialog.Builder(requireContext())
-        with (builder){
-            setTitle("Search Result")
-            setItems(tracks) {
-                _, item ->
-                addTrackToPlaylist(tracks[item])
-                Snackbar.make(view, "${tracks[item]}", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+    }
+
+    private suspend fun addTrackToPlaylist(
+        track: String,
+        api: SpotifyClientApi,
+        trackUri: PlayableUri,
+        view: View
+    ) {
+        val playlistName = "Song of the Day app"
+        var sotdPlaylistId: String? = null
+
+        withContext(Dispatchers.IO) {
+            api.playlists.getClientPlaylists().items.forEach {
+                if (it.name == playlistName && it.id != "") {
+                    sotdPlaylistId = it.id
+                }
+                return@forEach
             }
         }
-        builder.show()
+        Log.d("HomeFragment", "playlistId: $sotdPlaylistId")
 
-
-    }
-
-    private fun addTrackToPlaylist(track: String)
-    {
-        null
+        if (sotdPlaylistId != null) {
+            api.playlists.addPlayableToClientPlaylist(sotdPlaylistId!!, trackUri)
+            Log.i("HomeFragment", "Track: $track was added to the playlist")
+            Snackbar.make(view, "$track was added to the playlist", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+            return
+        }
+        Snackbar.make(view, "$track is already in the playlist", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show()
+        Log.i("HomeFragment", "Track: $track was a duplicate, not added to the playlist")
     }
 
 /*
@@ -177,7 +218,7 @@ class HomeFragment : Fragment(){
 */
 
     private fun checkLoggedIn(): Boolean {
-        if (auth.currentUser  == null) {
+        if (auth.currentUser == null) {
             binding.root.getViewById(R.id.fab_home).visibility = View.GONE
             return false
         }
